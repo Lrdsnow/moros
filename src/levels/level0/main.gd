3e3edd2756a9138303cb918e70b587b6ca5a0647
Lrdsnow <Lrdsnow101@gmail.com>
2023-06-10 05:31:15 -0700

    e

--- src/levels/level0/main.gd
+++ src/levels/level0/main.gd
@@ -1,7 +1,9 @@
 extends Node2D
 
 @export var player:PackedScene
+@export var clear_color:Color
 
 func _ready():
+	RenderingServer.set_default_clear_color(clear_color)
 	var player_inst = player.instantiate()
 	$spawn.add_child(player_inst)
--- src/other/player.gd
+++ src/other/player.gd
@@ -21,12 +21,16 @@ func _input(event):
 			$tex.flip_h=false
 			$tex.play("walking")
 		elif event.is_action_released("ui_left") or event.is_action_released("ui_right"):
+			$tex.stop()
 			$tex.play("idle")
 
 
 func _physics_process(delta):
 	velocity.y += gravity * delta
-	velocity.x = Input.get_axis("ui_left", "ui_right") * speed
+	if not $tex.animation == "idle":
+		velocity.x = Input.get_axis("ui_left", "ui_right") * speed
+	else:
+		velocity.x = 0
 	move_and_slide()
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = jump_speed
